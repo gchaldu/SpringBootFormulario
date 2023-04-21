a5f55ac1246b5852f8acb7566f7f257eca8f8081
Gabriel Chaldu <gabrielchaldu@gmail.com>
2023-04-21 09:50:12 -0300

    atributo objecto en el form

--- src/main/java/net/youtics/springboot/formularios/controllers/FormController.java
+++ src/main/java/net/youtics/springboot/formularios/controllers/FormController.java
@@ -6,11 +6,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+/*import org.springframework.web.bind.annotation.ModelAttribute;*/
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.HashMap;
-import java.util.Map;
+/*import java.util.HashMap;
+import java.util.Map;*/
 
 
 @Controller
@@ -35,12 +35,13 @@ public class FormController {
         model.addAttribute("titulo", "Resultado Usuario");
         if(result.hasErrors())
         {
-            Map<String, String> errores = new HashMap<>();
+            /*Map<String, String> errores = new HashMap<>();
 
             result.getFieldErrors().forEach(err->{
                 errores.put(err.getField(), "El campo: " + err.getField() + " " + err.getDefaultMessage());
             });
-            model.addAttribute("error", errores);
+            model.addAttribute("error", errores);*/
+
             return "form";
         }
         model.addAttribute("usuario", usuario);
