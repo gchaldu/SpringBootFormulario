f8511ceb4efd306a67444e5a10cd16ddca36d39a
Gabriel Chaldu <gabrielchaldu@gmail.com>
2023-04-21 10:08:56 -0300

    SessionAttributes

--- src/main/java/net/youtics/springboot/formularios/controllers/FormController.java
+++ src/main/java/net/youtics/springboot/formularios/controllers/FormController.java
@@ -8,18 +8,24 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 /*import org.springframework.web.bind.annotation.ModelAttribute;*/
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 /*import java.util.HashMap;
 import java.util.Map;*/
 
 
 @Controller
+@SessionAttributes("usuario")
 public class FormController {
 
     @GetMapping("/form")
     public String form(Model model)
     {
         Usuario usuario = new Usuario();
+        usuario.setApellido("Chaldu");
+        usuario.setNombre("Gabriel");
+        usuario.setIdentificador("1.2-3");
         model.addAttribute("titulo", "Crear Usuario");
         model.addAttribute("usuario", usuario);
         return "form";
@@ -30,22 +36,16 @@ public class FormController {
      * primero el objeto a validar y luego el result
      * */
     @PostMapping("/form")
-    public String procesar(@Valid Usuario usuario, BindingResult result, Model model)
+    public String procesar(@Valid Usuario usuario, BindingResult result, Model model, SessionStatus sessionStatus)
     {
         model.addAttribute("titulo", "Resultado Usuario");
         if(result.hasErrors())
         {
-            /*Map<String, String> errores = new HashMap<>();
-
-            result.getFieldErrors().forEach(err->{
-                errores.put(err.getField(), "El campo: " + err.getField() + " " + err.getDefaultMessage());
-            });
-            model.addAttribute("error", errores);*/
-
             return "form";
         }
         model.addAttribute("usuario", usuario);
-
+        /*limpia la session*/
+        sessionStatus.setComplete();
         return "resultado";
     }
 }
--- src/main/java/net/youtics/springboot/formularios/models/domain/Usuario.java
+++ src/main/java/net/youtics/springboot/formularios/models/domain/Usuario.java
@@ -1,9 +1,14 @@
 package net.youtics.springboot.formularios.models.domain;
 
 import jakarta.validation.constraints.NotEmpty;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 public class Usuario {
-
+    private String identificador;
+    @NotEmpty
+    private String nombre;
+    @NotEmpty
+    private String apellido;
     @NotEmpty
     private String username;
     @NotEmpty
@@ -33,5 +38,29 @@ public class Usuario {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getIdentificador() {
+        return identificador;
+    }
+
+    public void setIdentificador(String identificador) {
+        this.identificador = identificador;
     }
 }
